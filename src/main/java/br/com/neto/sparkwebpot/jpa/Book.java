a400f7bd16fbe6e6062d55f0967835e8fa968ff0
Valdemar Arantes Neto <valdemar.neto@certisign.com.br>
2022-05-04 21:23:12 -0300

    ...

---
+++ src/main/java/br/com/neto/sparkwebpot/jpa/Book.java
@@ -0,0 +1,63 @@
+package br.com.neto.sparkwebpot.jpa;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public final class Book {
+    private Integer id;
+    private String title;
+
+    public Book() {
+    }
+
+    public Book(Integer id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Book) obj;
+        return this.id == that.id &&
+                Objects.equals(this.title, that.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title);
+    }
+
+    @Override
+    public String toString() {
+        return "Book[" +
+                "id=" + id + ", " +
+                "title=" + title + ']';
+    }
+
+
+
+}
---
+++ src/main/java/br/com/neto/sparkwebpot/jpa/PersistenceUtils.java
@@ -0,0 +1,109 @@
+package br.com.neto.sparkwebpot.jpa;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.internal.SessionImpl;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.sql.Connection;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicIntegerArray;
+import java.util.stream.IntStream;
+
+public class PersistenceUtils {
+    private final static EntityManagerFactory entityManagerFactory;
+    private static final Logger log = LogManager.getLogger();
+    private static final AtomicInteger counter = new AtomicInteger(0);
+
+    static {
+        entityManagerFactory = Persistence.createEntityManagerFactory("AppUnit");
+    }
+
+    public static void main(String[] args) {
+//        saveAndList();
+        multiThreadTest();
+    }
+
+    protected static void create(EntityManager entityManager, Book book) {
+        entityManager.getTransaction().begin();
+        try {
+            entityManager.persist(book);
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
+            log.error("Error persisting Book", e);
+        }
+    }
+
+    protected static List findAll(EntityManager entityManager) {
+        return entityManager.createQuery("select b from Book b").getResultList();
+    }
+
+    private static void saveAndList() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        final var b = new Book();
+        b.setTitle("Bible");
+        log.info("b = {}", b);
+
+        create(entityManager, b);
+        log.info("After persist: b={}", b);
+
+        List resultList = findAll(entityManager);
+        log.info("resultList = {}", resultList);
+    }
+
+    private static void save() {
+
+        log.info("Saving: counter.incrementAndGet() = {}", counter.incrementAndGet());
+
+        final var b = new Book();
+        b.setTitle("Bible");
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        try {
+            create(entityManager, b);
+        } catch (Exception e) {
+            log.error("", e);
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    private static void openTrx() {
+        log.info("openTrx: counter.incrementAndGet() = {}", counter.incrementAndGet());
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        Connection connection = ((SessionImpl) entityManager).connection();
+        log.info("connection = {}", connection);
+    }
+
+    private static void multiThreadTest() {
+
+        Runnable suplier = PersistenceUtils::openTrx;
+
+        Executor executor = Executors.newFixedThreadPool(150);
+
+        var initTime = System.nanoTime();
+        var finalTime = initTime;
+        var futures = IntStream.range(0, 10)
+                .mapToObj((n) -> CompletableFuture.runAsync(suplier, executor))
+                .toList();
+
+        try {
+            CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).get();
+            finalTime = System.nanoTime();
+        } catch (Exception e) {
+            log.error("", e);
+        }
+
+        log.info("Encerrado: {} us", (finalTime - initTime) / 1000);
+        System.exit(0);
+    }
+
+}
