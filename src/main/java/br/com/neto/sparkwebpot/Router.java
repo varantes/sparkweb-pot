ea351c639232190df84205ef07b5cbbc3ce3c316
Valdemar Arantes Neto <142926+varantes@users.noreply.github.com>
2022-10-29 11:07:41 -0300

    Inclusão de medições de tempos de execução utilizando os filtros do Spark before e after.

--- src/main/java/br/com/neto/sparkwebpot/Router.java
+++ src/main/java/br/com/neto/sparkwebpot/Router.java
@@ -6,12 +6,16 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 import static spark.Spark.*;
 
 public class Router {
 
     private static final Logger log = LogManager.getLogger();
+    private static ThreadLocal<Long> longThreadLocal = new ThreadLocal<>();
+    private static AtomicLong atomicLong = new AtomicLong(0);
+
     private final OneService oneService;
     private final ObjectMapper mapper;
 
@@ -22,18 +26,24 @@ public class Router {
     }
 
     public void configure() {
-        before("/*", (req, res) -> log.info("{}: {}", req.requestMethod(), req.uri()));
+        before("/*", (req, res) -> {
+            longThreadLocal.set(System.nanoTime());
+            log.info("REQUEST {}: {}", req.requestMethod(), req.uri());
+        });
 
         get("/", (req, res) -> "Working" );
         get("/person", (req, res) -> mapper.writeValueAsString(oneService.getPerson()) );
         get("/hi", (req, res) -> oneService.sayHi());
 
         after("/*", (req, res) -> {
+            Object respBody;
             try {
-                log.info(mapper.readValue(res.body(), Map.class));
+                respBody = mapper.readValue(res.body(), Map.class);
             } catch (Exception e) {
-                log.info(res.body());
+                respBody = res.body();
             }
+            log.info("RESPONSE body={} [after {} ms]", respBody, ((double)(System.nanoTime() - longThreadLocal.get())) / 1000_000);
+            longThreadLocal.remove();
         });
     }
 }
